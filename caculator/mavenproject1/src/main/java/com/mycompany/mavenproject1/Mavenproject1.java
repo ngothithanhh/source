f521e18d5da175e42dae0d65ad67d80f1eb2e597
Ngô Thị Thanh <148693299+ngothithanhh@users.noreply.github.com>
2025-06-24 16:44:23 +0700

    caculator

---
+++ caculator/mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author ngoth
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        NewJFrame caculator = new NewJFrame();
+        caculator.setVisible(true);
+    }
+}
